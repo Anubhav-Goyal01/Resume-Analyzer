5bba06a6025f5b9ae460806f6e58c328157973e5
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-06-08 17:21:10 +0530

    Code refactoring

---
+++ utils.py
@@ -0,0 +1,70 @@
+import os
+import json
+import openai
+from langchain_community.document_loaders import PyPDFLoader
+
+
+def parseResume(file_path):
+    loader = PyPDFLoader(file_path)
+    content = loader.load()
+    return content
+
+
+def getResumeDetails(text, prompt):
+    client = openai.AzureOpenAI(
+        azure_endpoint = os.environ["AZURE_ENDPOINT_GPT_4"], 
+        api_key=os.environ["API_KEY_GPT_4"],  
+        api_version=os.environ["API_VERSION_GPT_4"],
+    )
+
+    completion = client.chat.completions.create(
+        model='sfslackbot',
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": f"""Here is the Resume Text:{text}"""}
+        ],
+        temperature=0.1,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
+    response = completion.choices[0].message.content
+    try:
+        starting_index = response.index("{")
+        ending_index = response.rindex("}") + 1
+        response = response[starting_index:ending_index]
+        response = json.loads(response)
+        return response
+    except:
+        return None
+
+
+def rankResumes(text, prompt, jobDescription):
+    client = openai.AzureOpenAI(
+        azure_endpoint = os.environ["AZURE_ENDPOINT_GPT_4"], 
+        api_key=os.environ["API_KEY_GPT_4"],  
+        api_version=os.environ["API_VERSION_GPT_4"],
+    )
+
+    completion = client.chat.completions.create(
+        model='sfslackbot',
+        messages=[
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": f"""Here are the resumes:{text} and the job description: {jobDescription}"""}
+        ],
+        temperature=0.1,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
+    response = completion.choices[0].message.content
+    try:
+        starting_index = response.index("{")
+        ending_index = response.rindex("}") + 1
+        response = response[starting_index:ending_index]
+        response = json.loads(response)
+        return response
+    except:
+        return None
