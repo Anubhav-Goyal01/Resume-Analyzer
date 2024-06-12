488d4c021302f0c2bc5e60b58b5792bce3776309
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-06-12 23:45:52 +0530

    Removed Second Assistant for now

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from Prompt.prompt import prompt, rankingPrompt, jobDescriptionPrompt, ParsingCheckPrompt
 load_dotenv()
 
-RESUME_DIR = os.path.join(os.path.dirname(__file__), 'Data')
+RESUME_DIR = os.path.join(os.path.dirname(__file__), 'Data', "Coffeee")
 resumes = os.listdir(RESUME_DIR)
 
 parsedResumes = []
@@ -20,23 +20,24 @@ for resume in resumes:
     details = getResumeDetails(text, prompt)
 
     # Check if the parsing is correct
-    check = checkParsing(details, ParsingCheckPrompt)
-    i = 0
-    while True:
-        if "yes" in check.lower():
-            break
-        else:
-            print("Parsing failed. Reparsing resume...")
-            details = getResumeDetails(text, prompt)
-            check = checkParsing(details, ParsingCheckPrompt)
-            print(check)
-            i += 1
-            if i > 3:
-                break
+    # check = checkParsing(details, ParsingCheckPrompt)
+    # i = 0
+    # while True:
+    #     if "yes" in check.lower():
+    #         break
+    #     else:
+    #         print("Parsing failed. Reparsing resume...")
+    #         details = getResumeDetails(text, prompt)
+    #         check = checkParsing(details, ParsingCheckPrompt)
+    #         print(check)
+    #         i += 1
+    #         if i > 3:
+    #             break
 
     
     if details:
         print("Parsing successful")
+        print(json.dumps(details, indent=4))
         parsedResumes.append(details)
         
     else:
